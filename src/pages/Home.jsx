ad94086b96da6a67536658b6759d9e923f494378
fpsnetodf <fps_netodf@hotmail.com>
2024-02-08 16:43:19 -0300

    vai part 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,22 @@
+import { useState, useEffect } from "react"
+
+const moviesURL = import.meta.env.VITE_API
+const apikey = import.meta.env.VITE_API_KEY
+
+
 function Home(){
+    { /*{console.log(moviesURL, apikey)}*/ }
+    const [ topMovies, setTopMovies ] = useState([])
+    const getTopRatedMovies = async (url) => {
+        const res = await fetch(url)
+        const data = await res.json()
+
+        console.log(data)
+    }
+    useEffect(() =>{
+        const topRatedUrl = `${moviesURL}top_rated?${apikey}`
+        console.log(topRatedUrl)
+    },[])
  return (<>
  <div>Home</div>
  <br />
